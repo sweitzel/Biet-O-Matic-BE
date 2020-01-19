939730715b5ced0cf7523ee3b3cf14515f69241c
Sebastian Weitzel <sweitzel@users.noreply.github.com>
2020-01-19 20:11:48 +0100

    Remove some undesired files from hugo book theme

--- webpack.config.js
+++ webpack.config.js
@@ -120,11 +120,6 @@ var options = {
 };
 
 // always include source-map
-options.devtool = "inline-source-map";
-/*
-if (env.NODE_ENV === "development") {
-  options.devtool = "source-map";
-}
-*/
+options.devtool = "source-map";
 
 module.exports = options;
